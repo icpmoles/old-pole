3ff9ddf7d69c0ec20701dd2ede8976fabcae4324
Iacopo Moles <iacopo@icpmol.es>
2025-07-21 00:28:56 +0200

    refactoring
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>

---
+++ assets/js/searchv2.js
@@ -0,0 +1,84 @@
+var idx;
+var jslist;
+let url = '/index.json';
+var searchb = document.getElementById("searchbar");
+var searchres = document.getElementById("searchresults");
+var lastRes;
+var debugsymbol;
+
+
+fetch(url)
+.then(res => res.json())
+.then(js => jslist = js )
+.then(documents  =>  {
+  idx = lunr(function () {
+    this.ref('url')
+    this.field('title', { boost: 100 })
+    this.field('content')
+    this.field('tags', { boost: 50 })
+    documents.forEach(function (doc) {
+      this.add(doc)
+    }, this);
+
+    this.pipeline.add(
+	    lunr.trimmer,
+	    lunr.stopWordFilter,
+	    lunr.stemmer
+	  )
+    this.searchPipeline.add(
+	    lunr.stemmer
+	  )
+  })
+
+  
+  searchb.removeAttribute("disabled");
+  searchb.setAttribute("placeholder", "Insert words");
+
+
+
+  searchb.addEventListener("input", (event) => { 
+    while (searchres.firstChild) {
+      searchres.removeChild(searchres.firstChild);
+    }
+    if (event.target.value.length > 2) {
+        lastRes = idx.search(event.target.value)
+        lastRes.forEach(res =>{
+          jsEl =  jslist.filter((el) => el.url == res.ref)[0]; 
+          let completeURL = res.ref.concat("#:~:text=").concat(event.target.value)
+          let li = document.createElement("li");
+          let body = document.createElement("div"); 
+          let h2 = document.createElement("h1");
+          h2.append(jsEl.title)
+
+          let link = document.createElement("a");
+          link.setAttribute("href",completeURL)
+          link.append(h2);
+          var rawhtml
+          if (jsEl.hasRaw == true){
+            fetch(jsEl.raw)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                rawhtml = res.raw;
+                debugsymbol = res.raw
+            } )
+            console.log(rawhtml)
+            body.innerHTML=rawhtml
+           
+          }
+
+          
+          li.appendChild(link) 
+          li.appendChild(body) 
+          searchres.appendChild(li)
+        });
+    }
+
+
+  })
+
+} )
+.catch(err => console.log(err));
+
+
+
